39ac060c70eb14d030a623e5d4ca50ba6925cc19
Valeri Hristov <valeri.hristov@chaos.com>
2023-01-17 00:07:51 +0200

    1

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pathify
+
+Open files from path string literals. 
+
+## Features
+
+Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+
+For example if there is an image subfolder under your extension project workspace:
+
+![Pathify](images/pathify.gif)
+
+## Requirements
+
+No requirements
+
+## Extension Settings
+
+No settings - works out-of-the-box!
+
+## Release Notes
+
+### 1.0.0
+
+- Resolves absolute, relative and parent paths.
+
+---
---
+++ extension.js
@@ -0,0 +1,58 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs = require('fs')
+const path = require('path')
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let openFileOnPath = vscode.commands.registerCommand('pathify.openFileOnPath', function () {
+		// The code you place here will be executed every time your command is executed
+		let editor = vscode.window.activeTextEditor;
+		let regeTon = new RegExp(/(?=["'])(?:"[^"\\]*(?:\\[\s\S][^"\\]*)*"|'[^'\\]*(?:\\[\s\S][^'\\]*)*')/);
+		//Get the selection starting from the cursor position and searching for a regular expression (default search between quotes or double quotes)
+		let range = editor.document.getWordRangeAtPosition(editor.selection.active, regeTon);
+
+		if (range) {
+			var currentlyOpenTabfilePath = editor.document.fileName;
+			//Get the pure match against the regualr expression
+			let text = editor.document.getText(range);
+			let strippedFromQuotes = text.substring(1, text.length - 1);
+
+			var resolvedPath;
+
+			if (strippedFromQuotes.startsWith("..") || strippedFromQuotes.startsWith('.')) {
+				// relative path
+				let segments = strippedFromQuotes.split("/");
+
+				resolvedPath = path.join(path.dirname(currentlyOpenTabfilePath), ...segments);
+			} else {
+				resolvedPath = strippedFromQuotes;
+			}
+
+			if (!fs.existsSync(resolvedPath)) {
+				return;
+			}
+
+			let url = vscode.Uri.parse('file:///' + resolvedPath);
+			vscode.commands.executeCommand('vscode.open', url);
+		}
+
+		
+	});
+
+	context.subscriptions.push(openFileOnPath);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
